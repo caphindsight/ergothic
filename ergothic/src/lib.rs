ceab4a6f972a3feec3c4550bcec06ac9b3881ce6
Cap. Hindsight <hindsight@yandex.com>
2018-07-08 15:19:31 +1000

    Minor adjustments to Cargo.

--- ergothic/src/lib.rs
+++ ergothic/src/lib.rs
@@ -1,3 +1,15 @@
+//! Ergothic is a collection of helpers for setting up and running distributed
+//! statistical Monte-Carlo simulations written in Rust. It is multi-purpose and
+//! will work for any statistical simulation based on Monte-Carlo or its
+//! variations (Metropolis-Hastings, etc.). However, its primary purpose is to
+//! ease the toil for simulating Quantum Field Theory on the lattice.
+//! Ergothic will perform routine tasks unrelated to the subject of your
+//! research for you, allowing you to focus on the code that matters.
+//! Simulations written with ergothic can run both in the single-threaded local
+//! environment, which is super easy to debug, and on clusters with tens of
+//! thousands of nodes. No code changes are required to scale your simulation up
+//! to any number, that technical part has already been taking care for you!
+
 extern crate argparse;
 extern crate bson;
 extern crate mongodb;
